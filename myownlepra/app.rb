003390f171b0ff9a74016771d1be73c10b94e4cf
shyssirixius <ivmikvik@gmail.com>
2016-08-08 12:12:57 +0300

    get and post to new page

--- myownlepra/app.rb
+++ myownlepra/app.rb
@@ -6,3 +6,17 @@ require 'sinatra/reloader'
 get '/' do
 	erb "Hello! <a href=\"https://github.com/bootstrap-ruby/sinatra-bootstrap\">Original</a> pattern has been modified for <a href=\"http://rubyschool.us/\">Ruby School</a>"			
 end
+
+get '/something' do
+  erb "Hello World"
+end
+
+get '/new' do
+	erb :new
+end
+
+post '/new' do
+	@post_text = params[:post_text]
+
+	erb "You typed #{@post_text}"
+end
